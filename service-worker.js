012dc478ed5c18fd3a4c4d37f3b1d93afd4b217b
jeetumanchanda <65504091+jeetumanchanda@users.noreply.github.com>
2025-08-13 19:04:25 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,12 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('clock-cache').then(cache => cache.addAll([
+  e.waitUntil(caches.open('neon-clock').then(cache => cache.addAll([
     './',
-    './index.html'
+    './index.html',
+    './manifest.json',
+    './icon.png'
   ])));
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
